48a506cbcff3a215186b42454e74e5ea2dbad828
abhiradadiya07 <113245680+abhiradadiya07@users.noreply.github.com>
2024-08-06 23:00:46 +0530

    frontend starter code for react using js with tialwind and shadcn also.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold text-center">
+      Starer code for react using vite with shadcn-tailwind and js.
+    </h1>
+  )
+}
+
+export default App
